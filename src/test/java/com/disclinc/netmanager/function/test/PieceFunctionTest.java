ef7c4004ee7b9daf54050f009506d74b2d1e8c39
agnaldoluizs <agnaldo@innovatium.com.br>
2014-04-16 18:38:24 -0300

    teste corrigido

--- src/test/java/com/disclinc/netmanager/function/test/PieceFunctionTest.java
+++ src/test/java/com/disclinc/netmanager/function/test/PieceFunctionTest.java
@@ -1,7 +1,6 @@
 package com.disclinc.netmanager.function.test;
 
 import static mLibrary.mFnc.$piece;
-import static mLibrary.mFnc.$setpiece;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
@@ -43,26 +42,6 @@ public class PieceFunctionTest {
 				$piece($piece(STRING_TEST, DELIMITER, from), " "));
 	}
 	
-	@Test
-	public void testSettingValueToPiecePosition() {
-		String string = "InterSystems" + DELIMITER + "NOVA STRING AQUI"
-				+ DELIMITER + "Cambridge" + DELIMITER + "MA 02142";
-		
-		assertEquals("Fail on setting a new value to a elements of the string pieced", string,
-				$setpiece(STRING_TEST, DELIMITER, to, "NOVA STRING AQUI"));
-	}
-	
-	@Test
-	public void testSettingValueToNullPiecePosition() {
-		assertEquals("Fail on setting a new value to a null string", null, $setpiece(null, DELIMITER, to, "NOVA STRING AQUI"));
-	}
-	
-	@Test
-	public void testSettingNullValueToPiecePosition() {
-		String string = "InterSystems" + DELIMITER + DELIMITER + "Cambridge" + DELIMITER + "MA 02142";
-		
-		assertEquals("Fail on setting a new value to a null string", string, $setpiece(STRING_TEST, DELIMITER, to, null));
-	}
 }
 
 
