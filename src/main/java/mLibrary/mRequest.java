70fd890c28001638fdf7816c574034d5320d0154
FernandoVianaMaia <123pin5>
2014-05-29 13:45:48 -0300

    correção do método getCgiEnv().

--- src/main/java/mLibrary/mRequest.java
+++ src/main/java/mLibrary/mRequest.java
@@ -8,53 +8,69 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 
 public class mRequest {
-	
+
 	private mData data = new mData();
 	private HttpServletRequest originalRequest;
 	private mContent content;
-	
+
 	public mRequest(HttpServletRequest request) {
 		originalRequest = request;
-	}	
- 
-	public Object getCgiEnvs(Object...key){			
-		return getCgiEnv(key[0], "");
+		populateRequest(String.valueOf(request.getQueryString()));
 	}
 	
+	public mRequest(HttpServletRequest request, String queryString) {
+		originalRequest = request;
+		populateRequest(queryString);
+	}
+	
+	private void populateRequest(String queryString) {
+		String[] atributos = queryString.split("&");
+		String[] parameter = null;
+		String key,value;
+		for (String attr : atributos) {
+			parameter = attr.split("=");
+			key = parameter[0];
+			value = (parameter.length == 1)?"":parameter[1];
+			setData(key, 1, value);
+		}
+	}
+	public Object getCgiEnvs(Object... key) {
+		return getCgiEnv(key[0], "");
+	}
+
 	public void setCgiEnvs(Map<String, String[]> cgiEnvs) {
 		throw new UnsupportedOperationException();
 	}
 
-	public mVar varData(Object... args){
+	public mVar varData(Object... args) {
 		mVar var = null;
-		
+
 		List<Object> list = new ArrayList<Object>();
 		list.add("data");
 		list.addAll(Arrays.asList(args));
 		args = list.toArray();
-		if(args!=null && args.length>0){
+		if (args != null && args.length > 0) {
 			var = new mVar(args, this.data);
 		}
 		return var;
 	}
-	
-	public Object getData(Object... args){
+
+	public Object getData(Object... args) {
 		return varData("data", args).get();
 	}
-	
-	
-	public void setData(Object subs, Object idx, Object value){
-		data.subs("data", subs, idx).set(value);			
+
+	public void setData(Object subs, Object idx, Object value) {
+		data.subs("data", subs, idx).set(value);
 	}
 
 	public void killData(Object object, int i) {
 		throw new UnsupportedOperationException();
-		
+
 	}
 
 	public void setCgiEnvs(String string, Object object) {
 		throw new UnsupportedOperationException();
-		
+
 	}
 
 	public mContent getContent() {
@@ -66,38 +82,92 @@ public class mRequest {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	public Object getCgiEnv(Object string, Object pDefault) {
 		String cgiVarName = String.valueOf(string);
-		switch ( cgiVarName ) {
-		case "AUTH_TYPE": return originalRequest.getAuthType();
-		case "CONTENT_LENGTH": return originalRequest.getContentLength();
-		case "CONTEXT_PATH": return originalRequest.getContextPath();
-		case "CONTENT_TYPE": return originalRequest.getContentType();
-		case "PATH_INFO": return originalRequest.getPathInfo();
-		case "PATH_TRANSLATED": return originalRequest.getPathTranslated();
-		case "QUERY_STRING": return originalRequest.getQueryString();
-		case "REMOTE_ADDR": return originalRequest.getRemoteAddr();
-		case "REMOTE_HOST": return originalRequest.getRemoteHost();
-		case "REMOTE_USER": return originalRequest.getRemoteUser();
-		case "REQUEST_METHOD": return originalRequest.getMethod();
-		case "SCRIPT_NAME": return originalRequest.getServletPath();
-		case "SERVER_NAME": return originalRequest.getServerName();
-		case "SERVER_PORT": return originalRequest.getServerPort();
-		case "SERVER_PORT_SECURE": return originalRequest.isSecure();
-		case "SERVER_PROTOCOL": return originalRequest.getProtocol();
-		case "SERVER_SOFTWARE": return originalRequest.getHeader("Server-Software");
-		case "HTTP_ACCEPT": return originalRequest.getHeader("Accept");
-		case "HTTP_ACCEPT_CHARSET": return originalRequest.getHeader("Accept-Charset");
-		case "HTTP_ACCEPT_ENCODING": return originalRequest.getHeader("Accept-Encoding");
-		case "HTTP_ACCEPT_LANGUAGE": return originalRequest.getHeader("Accept-Language");
-		case "HTTP_CONNECTION": return originalRequest.getHeader("Connection");
-		case "HTTP_COOKIE": return originalRequest.getHeader("Cookie");
-		case "HTTP_HOST": return originalRequest.getHeader("Host");
-		case "HTTP_REFERER": return originalRequest.getHeader("Referer");
-		case "HTTP_USER_AGENT": return originalRequest.getHeader("User-Agent");
+		Object result = pDefault;
+		switch (cgiVarName) {
+		case "AUTH_TYPE":
+			result = originalRequest.getAuthType();
+			break;
+		case "CONTENT_LENGTH":
+			result = originalRequest.getContentLength();
+			break;
+		case "CONTEXT_PATH":
+			result = originalRequest.getContextPath();
+			break;
+		case "CONTENT_TYPE":
+			result = originalRequest.getContentType();
+			break;
+		case "PATH_INFO":
+			result = originalRequest.getPathInfo();
+			break;
+		case "PATH_TRANSLATED":
+			result = originalRequest.getPathTranslated();
+			break;
+		case "QUERY_STRING":
+			result = originalRequest.getQueryString();
+			break;
+		case "REMOTE_ADDR":
+			result = originalRequest.getRemoteAddr();
+			break;
+		case "REMOTE_HOST":
+			result = originalRequest.getRemoteHost();
+			break;
+		case "REMOTE_USER":
+			result = originalRequest.getRemoteUser();
+			break;
+		case "REQUEST_METHOD":
+			result = originalRequest.getMethod();
+			break;
+		case "SCRIPT_NAME":
+			result = originalRequest.getServletPath();
+			break;
+		case "SERVER_NAME":
+			result = originalRequest.getServerName();
+			break;
+		case "SERVER_PORT":
+			result = originalRequest.getServerPort();
+			break;
+		case "SERVER_PORT_SECURE":
+			result = originalRequest.isSecure();
+			break;
+		case "SERVER_PROTOCOL":
+			result = originalRequest.getProtocol();
+			break;
+		case "SERVER_SOFTWARE":
+			result = originalRequest.getHeader("Server-Software");
+			break;
+		case "HTTP_ACCEPT":
+			result = originalRequest.getHeader("Accept");
+			break;
+		case "HTTP_ACCEPT_CHARSET":
+			result = originalRequest.getHeader("Accept-Charset");
+			break;
+		case "HTTP_ACCEPT_ENCODING":
+			result = originalRequest.getHeader("Accept-Encoding");
+			break;
+		case "HTTP_ACCEPT_LANGUAGE":
+			result = originalRequest.getHeader("Accept-Language");
+			break;
+		case "HTTP_CONNECTION":
+			result = originalRequest.getHeader("Connection");
+			break;
+		case "HTTP_COOKIE":
+			result = originalRequest.getHeader("Cookie");
+			break;
+		case "HTTP_HOST":
+			result = originalRequest.getHeader("Host");
+			break;
+		case "HTTP_REFERER":
+			result = originalRequest.getHeader("Referer");
+			break;
+		case "HTTP_USER_AGENT":
+			result = originalRequest.getHeader("User-Agent");
+			break;
+		default:
+			break;
+		}
+		return (result != null) ? result : pDefault;
 	}
-	
-		return null;
-	}	
 }
